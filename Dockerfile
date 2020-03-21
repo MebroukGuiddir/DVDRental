d8e7694706fb1f01c2408dcf6da32ef96f7273ae
mg <guiddir.mebrouk@gmail.com>
2020-03-21 14:42:00 +0100

    update main.java

--- Dockerfile
+++ Dockerfile
@@ -3,13 +3,13 @@
 FROM maven:3.6.0-jdk-11-slim AS build
 COPY src /home/app/src
 COPY pom.xml /home/app
-RUN mvn -f /home/app/pom.xml clean package
-RUN ls -R /home/app
+RUN mvn -f /home/app/pom.xml clean install assembly:single
+
 #
 # Package stage
 #
 FROM openjdk:11-jre-slim
-COPY --from=build /home/app/target/dvdRental-1.0-SNAPSHOT.jar /usr/local/lib/dvdRental.jar
+COPY --from=build /home/app/target/dvdRental-1.0-SNAPSHOT-jar-with-dependencies.jar /usr/local/lib/dvdRental.jar
 EXPOSE 8080
 ENTRYPOINT ["java","-jar","/usr/local/lib/dvdRental.jar"]
 
--- src/main/java/fr/univ/tln/mguiddir/App.java
+++ src/main/java/fr/univ/tln/mguiddir/App.java
@@ -1,13 +1,28 @@
 package fr.univ.tln.mguiddir;
-
+import java.sql.*;
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+
+
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://172.17.0.2:5432/dvdrental", "postgres", "postgres")) {
+
+
+
+            System.out.println("Connected to PostgreSQL database!");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM film");
+            while (resultSet.next()) {
+                System.out.println("id:"+resultSet.getString("film_id")+"  titre : " + resultSet.getString("title"));
+            }
+
+        } catch (SQLException  e) {
+            System.out.println("Connection failure.");
+            e.printStackTrace();
+        }
     }
 }
